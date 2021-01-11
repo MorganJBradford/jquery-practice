ca6ca771d6eba6a6f9e9bc59b746010862074306
Lo-GR <diamondintheroth@gmail.com>
2021-01-11 14:29:52 -0800

    add Jqueryscripts.

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+JQuery("h1").click(function() {
+  alert("This is a header.")
+});
+
+JQuery("p").click(function(){
+  alert("This is a paragraph.");
+});
+
+JQuery("img").click(function(){
+  alert("this is an image.");
+});
